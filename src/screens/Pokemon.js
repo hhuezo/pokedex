abac60d1fbf2600cce199c20c9e60cd1d933bcbe
hhuezo <hugo.alex.huezo@gmail.com>
2022-10-06 14:53:38 -0600

    modificacion de barra de navegacion

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { ScrollView } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import { getPokemonDetailsApi } from "../api/pokemon";
 import Header from "../components/pokemon/Header";
 import Type from "../components/pokemon/Type";
@@ -14,7 +15,20 @@ export default function Pokemon(props) {
   } = props;
   const [pokemon, setPokemon] = useState(null);
 
-
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (
+        <Icon
+          name="arrow-left"
+          color="#fff"
+          size={20}
+          style={{ marginLeft: 20 }}
+          onPress={navigation.goBack}
+        />
+      ),
+    });
+  }, [navigation, params]);
 
   useEffect(() => {
     (async () => {
