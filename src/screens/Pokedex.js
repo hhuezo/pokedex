e4b14527b39284239318fe4d8a88a76104ab54da
hhuezo <hugo.alex.huezo@gmail.com>
2022-10-05 22:07:03 -0600

    obteniendo pokemon api

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { SafeAreaView, Text } from "react-native";
+import { getPokemonsApi } from "../api/pokemon";
 
 export default function Pokedex() {
+
+  //para cargar con la vista
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  //llamado a api
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
