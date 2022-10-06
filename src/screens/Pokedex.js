d121074a3308f3453e1ffd6b57f324facc146118
hhuezo <hugo.alex.huezo@gmail.com>
2022-10-05 22:39:23 -0600

    list nombres pokemon 7

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView } from "react-native";
 import { getPokemonsApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
+import PokemonList from "../components/PokemonList";
 
 export default function Pokedex() {
   const [pokemons, setPokemons] = useState([]);
@@ -42,7 +43,7 @@ export default function Pokedex() {
   };
   return (
     <SafeAreaView>
-      <Text>Pokedex</Text>
+      <PokemonList pokemons={pokemons} />
     </SafeAreaView>
   );
 }
